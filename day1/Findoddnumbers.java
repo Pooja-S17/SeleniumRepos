857dc13d6ca07bdf87d53596598d174b63dba267
Pooja-S17 <145131557+Pooja-S17@users.noreply.github.com>
2023-09-15 19:31:32 +0530

    Add files via upload
    Day1

---
+++ day1/Findoddnumbers.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Findoddnumbers {
+
+	public static void main(String args[]) {
+		int num = 10;
+		System.out.print("odd numbers from 1 to" + num+ ":" );
+				for (int i =1;i<=num;i++)
+				{
+				if (i%2!=0)
+				{
+		System.out.print(i+" ");
+				}
+				}
+
+}
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Firefox {
+
+	
+	public static void main(String[] args) {
+		
+	float browserversion = 100.2f;
+	System.out.println("the browser version is::"+browserversion);
+	
+	String browsername = "Firefox";
+	System.out.println("the browsername:"+browsername);
+	
+	boolean isVisible = true;
+	System.out.println("the broser is visible:"+isVisible);
+	
+	int releaseyear = 1998;
+	System.out.println("the release year of browser is:"+releaseyear);
+	
+	char browserlogo = 'f';
+	System.out.println("the browser logo :"+browserlogo);
+
+	}
+
+}
